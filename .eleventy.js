645aaa19bd3720df20199d143040eecd64cb9a5b
Ilya Schmitt <mr.schmitt@hotmail.de>
2022-01-27 22:16:48 +0100

    added layout

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig){
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/admin")
-
+    eleventyConfig.addPassthroughCopy("imgages");
     return{
         dir: {
             input: "src",
